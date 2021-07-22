66a5c130c3727651b7b18f2aaeea11c18a4c4ebe
Ibrahim Madi <ibrahim.s.m.2010@gmail.com>
2021-07-22 23:41:15 +0300

    first commit quran

---
+++ pages/_surah/page/_lang/_id.vue
@@ -0,0 +1,319 @@
+<template>
+  <div>
+    <b-container fluid class="p-0 mt-2">
+      <b-row class="justify-content-center overflow-auto text-center ">
+        <div class="col-6 col-lg-4 col-md-6 col-sm-12  ">
+          <div id="audio" class="player-wrapper">
+            <AudioPlayer
+              :status="!!ayahaAudio"
+              :ayah="ayahNumber"
+              :file="ayahaAudio"
+              :get-quran-audio="getQuranAudio"
+              :auto-play="true"
+            ></AudioPlayer>
+          </div>
+        </div>
+      </b-row>
+      <b-row
+        class="justify-content-center  mb-5 overflow-auto quran-banner" ref="app"
+        :style="{ height: scrollerHeight }"
+      >
+        <!-- Content here -->
+
+        <!-- <div class="col-12 col-lg-4 col-md-6 col-sm-12 frame h-100 " :style="{height: scrollerHeight}" @mouseenter="mouseEnter" @mousemove="mouseMove" @mouseleave="mouseLeave"  > -->
+        <div class="col-6 col-lg-4 col-md-6 col-sm-12 frame h-100 " v-if="surah">
+          <!-- <h1 v-if="surah">{{surah.name}}</h1> -->
+          <!-- <b-card bg-variant="white" :header="surah.name" text-variant="dark" class="text-center h-100  border border-warning p-2  mt-4 ">
+        <b-card-text class="h-100 mr-2 ml-2 text-justify"> -->
+          <div class="col pr-5 pl-5 pb-2 pt-3 mt-4 mb-5 text-justify">
+            <p class="mt-0" v-once>
+              {{ surah.surahs[$route.params.surah].name }}
+            </p>
+            <span class="p-0" v-for="(ayah, index) in surah.ayahs" :key="index">
+              <p v-if="ayah.numberInSurah === 1">
+                {{ ayah.text }}
+                <b-badge variant="warning">{{ ayah.numberInSurah }}</b-badge>
+              </p>
+
+              <template v-else>
+                <span  :class="{'ayaha-active-color':ayah.number === ayahNumber}">{{ ayah.text }}</span>
+                <b-badge variant="warning">{{ ayah.numberInSurah }}</b-badge>
+                <svg
+                  v-if="ayah.sajda"
+                  width="20"
+                  height="20"
+                  id="svg874"
+                  style="enable-background:new 0 0 512 512;"
+                  version="1.1"
+                  viewBox="0 0 32 32"
+                  xml:space="preserve"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:cc="http://creativecommons.org/ns#"
+                  xmlns:dc="http://purl.org/dc/elements/1.1/"
+                  xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+                  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                  xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+                  xmlns:svg="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                >
+                  <g id="layer5">
+                    <g id="path212">
+                      <path
+                        d="M6,30c-0.6,0-1-0.4-1-1V3c0-0.6,0.4-1,1-1s1,0.4,1,1v26C7,29.6,6.6,30,6,30z"
+                      />
+                    </g>
+                    <g id="path1019">
+                      <path
+                        d="M26,30c-0.6,0-1-0.4-1-1V3c0-0.6,0.4-1,1-1s1,0.4,1,1v26C27,29.6,26.6,30,26,30z"
+                      />
+                    </g>
+                    <g id="path1021">
+                      <path
+                        d="M26,7H6C5.4,7,5,6.6,5,6s0.4-1,1-1h20c0.6,0,1,0.4,1,1S26.6,7,26,7z"
+                      />
+                    </g>
+                    <g id="path1023">
+                      <path
+                        d="M26,27H6c-0.6,0-1-0.4-1-1s0.4-1,1-1h20c0.6,0,1,0.4,1,1S26.6,27,26,27z"
+                      />
+                    </g>
+                    <g id="path1025">
+                      <path
+                        d="M10,7C9.4,7,9,6.6,9,6V3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C11,6.6,10.6,7,10,7z"
+                      />
+                    </g>
+                    <g id="path1027">
+                      <path
+                        d="M18,7c-0.6,0-1-0.4-1-1V3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C19,6.6,18.6,7,18,7z"
+                      />
+                    </g>
+                    <g id="path1035">
+                      <path
+                        d="M14,7c-0.6,0-1-0.4-1-1V3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C15,6.6,14.6,7,14,7z"
+                      />
+                    </g>
+                    <g id="path1037">
+                      <path
+                        d="M22,7c-0.6,0-1-0.4-1-1V3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C23,6.6,22.6,7,22,7z"
+                      />
+                    </g>
+                    <g id="path1039">
+                      <path
+                        d="M10,30c-0.6,0-1-0.4-1-1v-3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C11,29.6,10.6,30,10,30z"
+                      />
+                    </g>
+                    <g id="path1041">
+                      <path
+                        d="M18,30c-0.6,0-1-0.4-1-1v-3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C19,29.6,18.6,30,18,30z"
+                      />
+                    </g>
+                    <g id="path1043">
+                      <path
+                        d="M14,30c-0.6,0-1-0.4-1-1v-3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C15,29.6,14.6,30,14,30z"
+                      />
+                    </g>
+                    <g id="path1045">
+                      <path
+                        d="M22,30c-0.6,0-1-0.4-1-1v-3c0-0.6,0.4-1,1-1s1,0.4,1,1v3C23,29.6,22.6,30,22,30z"
+                      />
+                    </g>
+                    <g id="path1047">
+                      <path
+                        d="M26,11H6c-0.6,0-1-0.4-1-1s0.4-1,1-1h20c0.6,0,1,0.4,1,1S26.6,11,26,11z"
+                      />
+                    </g>
+                    <g id="path1049">
+                      <path
+                        d="M26,23H6c-0.6,0-1-0.4-1-1s0.4-1,1-1h20c0.6,0,1,0.4,1,1S26.6,23,26,23z"
+                      />
+                    </g>
+                    <g id="path1051">
+                      <path
+                        d="M11,23c-0.6,0-1-0.4-1-1v-6c0-2.6,4.4-3.7,5.8-4c0.5-0.1,1.1,0.2,1.2,0.8c0.1,0.5-0.2,1.1-0.8,1.2c-2.1,0.4-4.2,1.4-4.2,2    v6C12,22.6,11.6,23,11,23z"
+                      />
+                    </g>
+                    <g id="path1053">
+                      <path
+                        d="M21,23c-0.6,0-1-0.4-1-1v-6c0-0.6-2.1-1.6-4.2-2c-0.5-0.1-0.9-0.6-0.8-1.2c0.1-0.5,0.6-0.9,1.2-0.8c1.4,0.3,5.8,1.4,5.8,4    v6C22,22.6,21.6,23,21,23z"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </template>
+            </span>
+              <div class="col text-center">
+                                <b-badge variant="info" class=" 
+                ">{{surah.number}}</b-badge>
+              </div>
+
+          </div>
+          <!-- </b-card-text>
+      </b-card>  -->
+        </div>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import AudioPlayer from "@/components/player";
+export default {
+  watch: {
+    scrollerHeight: { 
+     immediate:true,
+     handler:function(newValue){
+       console.log(newValue);
+     },
+    },
+    
+  },
+  components: {
+    AudioPlayer
+  },
+  data() {
+    return {
+      ayahaAudio: "",
+      ayahaAudioStatus: false,
+      ayahNumber: "",
+      surah:'',
+      scrollerHeight:555,
+    };
+  },
+  async asyncData({ $axios, params }) {
+    try {
+      const response = await $axios.$get(
+        `http://api.alquran.cloud/v1/page/${params.id}/${params.lang}.asad`
+      );
+      // console.log(response);
+      return { surah: response.data };
+    } catch (err) {
+      // if (err.response.status === 404) {
+      //   error({ statusCode: 404, message: "Design not found" });
+      // } else {
+      //   error({ statusCode: 500, message: "Internal server error" });
+      // }
+    }
+  },
+  computed: {
+    // scroll area height
+    // (business needs: phone screen height minus the height of the head and the bottom of the tabbar title, of course, two highly dynamic can also get in)
+    // scrollerHeight: function() {
+    //   if (process.client) {
+    //     return window.innerHeight - 46 - 50 + "px";
+    //   }
+    // }
+  },
+  mounted() {
+    if(this.surah)
+    this.getQuranAudio(this.surah.ayahs[0].number);
+
+    const that = this;
+    window.onresize = () =>{
+      return (()=>{
+        that.scrollerHeight = that.$refs.app.clientHeight;
+      });
+    };
+  },
+  methods: {
+    getQuranAudio(ayahNum) {
+      this.ayahNumber = ayahNum;
+    //  var t =  this.surah.ayahs.filter(function(item , index){
+    //     if (Number(item.number) === Number(ayahNum)) {
+    //       return true;
+    //       }
+    //   });
+      
+
+    if (this.surah.ayahs.filter(e => Number(e.number) === Number(ayahNum)).length > 0) {
+        this.ayahaAudioStatus = false;
+        this.$axios
+        .$get(`http://api.alquran.cloud/v1/ayah/${ayahNum}/ar.alafasy`)
+        .then(
+        res => (
+          (this.ayahaAudio = res.data.audio), (this.ayahaAudioStatus = true)
+        )
+        )
+        .catch(e => console.log(e))
+        .finally(() => console.log(this));
+
+    }else{
+    this.getPage(this.$route.params.surah,this.$route.params.lang,Number(this.$route.params.id)+1);
+      console.log('noneee');
+
+    }
+    //   if(t)
+   
+    },
+        getPage(surah ,language,id ) {
+          console.log(surah ,id , language);
+          this.$router.push({ name: 'surah-page-lang-id', params: { surah:surah ,lang:language,id:id} });
+        // this.$axios
+        //   .$get(
+          //   `http://api.alquran.cloud/v1/page/${page}/${this.$route.params.lang}.asad`
+          
+      //     )
+      //   .then(
+      //     res => (
+      //       (this.$router.push({ name: 'user', params: { surah:surah ,lang:language} }))
+      //     )
+      //   )
+      //   .catch(e => console.log(e))
+      //   .finally(() => console.log(this));
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+.card-body {
+  padding: 0px !important;
+  line-height: 2;
+}
+
+.frame {
+  background-image: url(~/assets/8fac60ef09ce30114277835318b75fa3.png);
+  background-size: 100% 100%;
+  font-family: "Al Qalam Quran";
+  src: url("~/assets/Al Qalam Quran.ttf");
+}
+@font-face {
+  font-family: "Al Qalam Quran";
+  src: url("~/assets/Al Qalam Quran.ttf");
+  font-weight: bold;
+  font-style: italic, oblique;
+}
+
+.ayaha-active-color{
+  background-color: aquamarine;
+}
+</style>
