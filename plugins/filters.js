574dc39e3d8dd6577eb31924a582999499cef948
Ibrahim Madi <ibrahim.s.m.2010@gmail.com>
2021-08-22 00:05:03 +0300

    filter

--- plugins/filters.js
+++ plugins/filters.js
@@ -3,13 +3,12 @@ import Vue from 'vue'
 Vue.filter('textColorNameAllah', (value) => {
   var res = value;
   if(res != ''){
-    var keywords = ["رَبِّهِمْ","رَبِّ","لِلَّهِ","اللَّهِ"];
+    var keywords = ["رَبِّهِمْ","لِلَّهِ","بِاللَّهِ","اللَّهِ","رَبِّكَ","رَبُّكَ","رَبِّنَا","بِرَبِّ","رَبِّكَ","رَبِّ","إِلَٰهِ","۩"];
     keywords.forEach(element => {
       var sp = `<span class="text-danger">${element}</span>`;
          res = res.toString().replace(element, sp);
       
     });
-    // var keywo = "اللَّهِ";
   }
   
   return res; 
